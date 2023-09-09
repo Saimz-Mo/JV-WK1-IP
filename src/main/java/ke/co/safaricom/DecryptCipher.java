452aae1a61b15928091c87e9d1e88e5d2d38cff7
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-09-09 12:17:21 +0300

    decrypt constructor  added

--- src/main/java/ke/co/safaricom/DecryptCipher.java
+++ src/main/java/ke/co/safaricom/DecryptCipher.java
@@ -1,4 +1,18 @@
 package ke.co.safaricom;
 
 public class DecryptCipher {
+    Character[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',};
+
+    private  char c;
+    private int key;
+
+    public DecryptCipher() {
+
+    }
+
+    public DecryptCipher(char c, int key) {
+        this.c = c;
+        this.key = key;
+    }
+
 }

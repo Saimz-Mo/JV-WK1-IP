c46e393415529cc7b7e397e45a5f5e3e5a439a83
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-09-09 12:42:09 +0300

    menu 2 updated on main

--- src/main/java/ke/co/safaricom/Main.java
+++ src/main/java/ke/co/safaricom/Main.java
@@ -16,7 +16,37 @@ public class Main {
             int keyInput = in.nextInt();
             String output = "";
 
+            for(int i = 0; i < input.length(); i++){
+                EncryptCipher encoding = new EncryptCipher();
+                encoding.setKey(keyInput);
+                encoding.setC(input.charAt(i));
+                output += encoding.encrypt();
+            }
 
+            System.out.println("Your encrypted message is: "+output);
+
+
+        }
+
+        else if (menu == 2){
+            System. out. println("Please enter a message to be decrypted: ");
+            String input = in.next();
+            System. out. println("Please enter a number to be your decryption key: ");
+            int keyInput = in.nextInt();
+            String output = "";
+            for(int i = 0; i < input.length(); i++){
+                DecryptCipher encoding = new DecryptCipher();
+                encoding.setKey(keyInput);
+                encoding.setC(input.charAt(i));
+                output += encoding.decrypt();
+            }
+            System.out.println("Your decrypted message is: "+output);
+
+        }
+
+        else
+        {
+            System.out.println("You should select 1 or 2");
         }
 
 

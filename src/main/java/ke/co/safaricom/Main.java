eaf7f1be4dfb2adf4b7cb951877fe66dcb3739fb
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-09-09 11:43:56 +0300

    scanner input added

--- src/main/java/ke/co/safaricom/Main.java
+++ src/main/java/ke/co/safaricom/Main.java
@@ -1,19 +1,24 @@
 package ke.co.safaricom;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("1: Encryption\n2: Decryption");
+        int menu = in.nextInt();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        if (menu == 1){
+            System.out.println("Please enter a message to be encrypted: ");
+            String input = in.next();
+            System.out.println("Please enter a number to be your shift key: ");
+            int keyInput = in.nextInt();
+            String output = "";
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            
+        }
+
+
         }
     }
-}

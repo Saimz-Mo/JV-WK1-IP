719da5a79132cae23a0ab15d56d4200c79679bbb
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-09-09 12:20:07 +0300

    decrypt logic  added

--- src/main/java/ke/co/safaricom/DecryptCipher.java
+++ src/main/java/ke/co/safaricom/DecryptCipher.java
@@ -1,5 +1,7 @@
 package ke.co.safaricom;
 
+import java.util.Arrays;
+
 public class DecryptCipher {
     Character[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',};
 
@@ -37,6 +39,31 @@ public class DecryptCipher {
 
     public void setKey(int key) {
         this.key = key;
+    }
+
+    public char decrypt() {
+        int index = Arrays.asList(alphabet).indexOf(c);
+        int newIndex = (index - key) % 26;
+
+        if (newIndex < 0) {
+            newIndex = 26 + newIndex;
+
+        }
+
+        if (c == ' ') {
+            return ' ';
+        }
+
+        if (c >= '0' && c <= '9') {
+            return c;
+        }
+
+        if (c == '.' || c == ',' || c == '!' || c == '?' || c == ';' || c == ':' || c == '\'' || c == '\"' || c == '(' || c == ')' || c == '[' || c == ']' || c == '{' || c == '}' || c == '-' || c == '_' || c == '+' || c == '=' || c == '*' || c == '/' || c == '&' || c == '^' || c == '%' || c == '$' || c == '#' || c == '@' || c == '~' || c == '`' || c == '<' || c == '>' || c == '|' || c == '\\' || c == '\t' || c == '\n' || c == '\r') {
+            return  c;
+        }
+
+        return alphabet[newIndex];
+
     }
 
 
